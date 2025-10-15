439e94feaa1ab667d7130e07e3b9bc208352099e
Emanuel Lopez <ema2805pro@gmail.com>
2025-10-15 18:20:42 -0500

    Agregar comentario sobre la posibilidad de usar un objeto para titulos con acentos

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,6 @@
 const titulos = ["Index"]
 // titulos.push(...categorylist)
+// Se podia hacer un objeto que tuviera de clave la variable sin acentos y el valor con acentos
 let menu = [];
 for (let titulo of titulos) {
     menu.push(`<li><a href="./${titulo.toLowerCase().replaceAll(" ", "_")}.html">${titulo}</a></li>`);
